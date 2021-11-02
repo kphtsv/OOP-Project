f5ce62dc46af3ad9b8fbb57768f632c593565b32
kphtsv <k.phtsv@ya.ru>
2021-11-03 03:54:07 +0500

    Json parsing, minor code refactor

--- src/main/java/ru/betry/Main.java
+++ src/main/java/ru/betry/Main.java
@@ -1,9 +1,22 @@
 package ru.betry;
 
+import com.google.gson.GsonBuilder;
+
 public class Main {
     public static void main(String[] args)
         {
-                TelegramBotProject myTelegramBot = new TelegramBotProject();
-                myTelegramBot.run();
+            /*var gson = new GsonBuilder().setPrettyPrinting().create();
+            var str = "{\"foo\":123, \"bar\":432}";
+            var res = gson.fromJson(str, A.class);
+            System.out.println(res.foo);*/
+
+//            TelegramBotProject myTelegramBot = new TelegramBotProject();
+//            myTelegramBot.run();
+
+            var gson = new GsonBuilder().setPrettyPrinting().create();
+            var str = "{\"suggestions\": [{\"data\": 986803, \"value\": \"\\u041c\\u0415\\u041d-200208\"}]}";
+            var response = gson.fromJson(str, IdQueryResponse.class);
+            System.out.println(response.suggestions.get(0).data);
         }
 }
+
--- src/main/java/ru/betry/UrfuScheduleSocket.java
+++ src/main/java/ru/betry/UrfuScheduleSocket.java
@@ -1,5 +1,7 @@
 package ru.betry;
 
+import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -10,12 +12,10 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-// почему бы не static?
 public class UrfuScheduleSocket {
     public final static String ID_QUERY_URL;
     public final static String SCHEDULE_URL;
 
-    private static final Pattern groupIdRE;
     private static final Pattern weeklyTableRE;
     private static final Pattern weekdayInfoRE;
     private static final Pattern weekdayClassesRE;
@@ -26,7 +26,6 @@ public class UrfuScheduleSocket {
         ID_QUERY_URL = "https://urfu.ru/api/schedule/groups/suggest/?query=";
         SCHEDULE_URL = "https://urfu.ru/ru/students/study/schedule/#student/";
 
-        groupIdRE = Pattern.compile("(?<=\"data\": )\\d*(?=})");
         weeklyTableRE = Pattern.compile("(?s)<table class=\"shedule-group-table\">.*?</table>");
         weekdayInfoRE = Pattern.compile("(?s)<tr class=\"divide\">.*?<b>(?<date>.*?)</b>(?<schedule>.*?)<tr class=\"divide\">.*?</tr>");
         weekdayClassesRE = Pattern.compile("(?s)<tr class=\"shedule-weekday-row\">.*?</tr>");
@@ -37,7 +36,7 @@ public class UrfuScheduleSocket {
     private static String getPageContent(String url) throws IOException {
         URLConnection connection = null;
         connection =  new URL(url).openConnection();
-        Scanner scanner = new Scanner(connection.getInputStream());
+        var scanner = new Scanner(connection.getInputStream());
         scanner.useDelimiter("\\Z");
         String content = scanner.next();
         scanner.close();
@@ -51,16 +50,17 @@ public class UrfuScheduleSocket {
         }
     }
 
-    // TODO распарсить json
     private static String getWeekScheduleUrl(String group, Date date) throws IOException, InvalidGroupException {
-        String idQueryResponse = getPageContent(ID_QUERY_URL + group);;
-        Matcher matcher = groupIdRE.matcher(idQueryResponse);
+        var idQueryResponse = getPageContent(ID_QUERY_URL + group);
 
-        if (!matcher.find())
-            throw new InvalidGroupException("Invalid group");
+        var gson = new GsonBuilder().setPrettyPrinting().create();
+        var response = gson.fromJson(idQueryResponse, IdQueryResponse.class);
 
-        String id = matcher.group(0);
-        String dateFormatted = new SimpleDateFormat("yyyyMMdd").format(date);
+        if (response.suggestions.size() < 1)
+            throw new InvalidGroupException("Invalid group.");
+
+        var id = response.suggestions.get(0).data;
+        var dateFormatted = new SimpleDateFormat("yyyyMMdd").format(date);
 
         return SCHEDULE_URL + id + "/" + dateFormatted + "/"; // нужен ли последний + "/" ?
     }
@@ -82,26 +82,25 @@ public class UrfuScheduleSocket {
 
     // достаём данные из таблицы
     public static Object getScheduleByGroup(String group, Date date) throws InvalidGroupException, IOException {
-        String url = getWeekScheduleUrl(group, date);
-        String schedulePageContent = getPageContent(url);
+        var url = getWeekScheduleUrl(group, date);
+        var schedulePageContent = getPageContent(url);
 
         Matcher matcher = weeklyTableRE.matcher(schedulePageContent);
         if (!matcher.find())
             throw new IOException();
-        String tableContent = matcher.group(0);
+        var tableContent = matcher.group(0);
+
         return extractWeeklySchedule(tableContent);
     }
-
-
 
     // достаём все дни
     private static Object extractWeeklySchedule(String tableContent) {
         Matcher matcher = weekdayInfoRE.matcher(tableContent);
-        ArrayList<ArrayList<ClassInfo>> weeklySchedule = new ArrayList<>();
-        while (matcher.find()) { // информация только по одному дню недели
-            String weekdayInfo = matcher.group(0);
+        var weeklySchedule = new ArrayList<ArrayList<ClassInfo>>();
+        while (matcher.find()) {
+            var weekdayInfo = matcher.group(0);
             // String weekdayDate = matcher.group("date");
-            ArrayList<ClassInfo> weekdaySchedule = extractWeekdaySchedule(weekdayInfo);
+            var weekdaySchedule = extractWeekdaySchedule(weekdayInfo);
             weeklySchedule.add(weekdaySchedule);
         }
         return weeklySchedule;
@@ -109,11 +108,11 @@ public class UrfuScheduleSocket {
 
     // достаём все пары
     private static ArrayList<ClassInfo> extractWeekdaySchedule(String weekdayInfo) {
-        ArrayList<ClassInfo> weekdaySchedule = new ArrayList<>();
-        Matcher matcher = weekdayClassesRE.matcher(weekdayInfo);
+        var weekdaySchedule = new ArrayList<ClassInfo>();
+        var matcher = weekdayClassesRE.matcher(weekdayInfo);
         while (matcher.find()) {
-            String classInfoRaw = matcher.group(0);
-            ClassInfo classInfo = extractClassInfo(classInfoRaw); // TODO пиздец глаза выпали нахуй ))0)
+            var classInfoRaw = matcher.group(0);
+            var classInfo = extractClassInfo(classInfoRaw); // TODO пиздец глаза выпали нахуй ))0)
             weekdaySchedule.add(classInfo);
         }
         return weekdaySchedule;
@@ -123,10 +122,10 @@ public class UrfuScheduleSocket {
     private static ClassInfo extractClassInfo(String classInfoRaw) {
         Matcher matcher = classInfoRE.matcher(classInfoRaw);
         if (matcher.find()) {
-            String order = matcher.group("order");
-            String time = matcher.group("time");
-            String name = matcher.group("name");
-            String extraInfo = formatRawExtraClassInfo(matcher.group("extraInfo"));
+            var order = matcher.group("order");
+            var time = matcher.group("time");
+            var name = matcher.group("name");
+            var extraInfo = formatRawExtraClassInfo(matcher.group("extraInfo"));
 
             return new ClassInfo(order, time, name, extraInfo);
         }
@@ -135,8 +134,8 @@ public class UrfuScheduleSocket {
     }
 
     private static String formatRawExtraClassInfo(String rawExtraInfo) {
-        Matcher matcher = extraInfoRe.matcher(rawExtraInfo);
-        StringBuilder result = new StringBuilder();
+        var matcher = extraInfoRe.matcher(rawExtraInfo);
+        var result = new StringBuilder();
         while (matcher.find()) {
             result.append(matcher.group(1));
             result.append(". ");
