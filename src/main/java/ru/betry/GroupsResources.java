6403cf6f1eb9fed74047f26a2451a233a529ae7d
ShalamovIvan <shalamov.ivan.02@Mail.ru>
2021-12-07 00:59:53 +0500

    Добавлены ресурсы групп

---
+++ src/main/java/ru/betry/GroupsResources.java
@@ -0,0 +1,66 @@
+package ru.betry;
+
+import java.util.HashMap;
+
+public class GroupsResources {
+    public HashMap<String, String> groups = new HashMap<String, String>();
+
+    public GroupsResources () {
+        groups.put("ймлн-101", "110206");
+        groups.put("ймлн-102", "110207");
+        groups.put("ймлн-103", "110208");
+        groups.put("ймлн-104", "110201");
+        groups.put("ймлн-105", "112201");
+        groups.put("ймлн-106", "112203");
+        groups.put("йа-101", "111015");
+        groups.put("йа-102", "111001");
+        groups.put("тр-101", "110801");
+        groups.put("тр-102", "110802");
+        groups.put("тр-103", "110809");
+        groups.put("тр-104", "110810");
+        groups.put("лло-101", "110101");
+        groups.put("лло-102", "110102");
+        groups.put("лло-103", "110705");
+        groups.put("лло-104", "113201");
+        groups.put("йм-201", "200206");
+        groups.put("йм-202", "200207");
+        groups.put("йм-203", "200208");
+        groups.put("йм-204", "200201");
+        groups.put("лн-201", "202201");
+        groups.put("лн-202", "202203");
+        groups.put("йа-201", "201015");
+        groups.put("тр-201", "200809");
+        groups.put("тр-202", "200810");
+        groups.put("тр-203", "200801");
+        groups.put("тр-204", "200802");
+        groups.put("лр-201", "200101");
+        groups.put("лр-202", "200102");
+        groups.put("лу-201", "200705");
+        groups.put("ол-201", "203201");
+        groups.put("лр-301", "390101");
+        groups.put("лр-302", "390102");
+        groups.put("лу-301", "390705");
+        groups.put("ол-301", "393201");
+        groups.put("йа-301", "390206");
+        groups.put("йм-301", "390206");
+        groups.put("йм-302", "390207");
+        groups.put("йм-303", "390208");
+        groups.put("йм-304", "390201");
+        groups.put("тр-301", "390809");
+        groups.put("тр-302", "390810");
+        groups.put("лн-301", "392201");
+        groups.put("лн-302", "392203");
+        groups.put("лр-401", "480101");
+        groups.put("лр-402", "480102");
+        groups.put("лу-401", "480705");
+        groups.put("йа-401", "481015");
+        groups.put("йм-401", "480206");
+        groups.put("йм-402", "480207");
+        groups.put("йм-403", "480208");
+        groups.put("тр-401", "480809");
+        groups.put("тр-402", "480810");
+        groups.put("лн-401", "482201");
+        groups.put("лн-402", "482203");
+        groups.put("ол-401", "483201");
+    }
+}
